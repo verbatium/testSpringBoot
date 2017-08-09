c22fe9438a1c50af1652f5ae917152ce254959b1
valerik <orhdposit@gmail.com>
2017-08-09 21:55:17 +0300

    Create test spring boot application

---
+++ src/main/java/ee/valja7/experiment/springboot/hello/Application.java
@@ -0,0 +1,36 @@
+package ee.valja7.experiment.springboot.hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+    public static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            LOG.info("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                LOG.info("Bean: {}", beanName);
+            }
+
+        };
+    }
+
+}
